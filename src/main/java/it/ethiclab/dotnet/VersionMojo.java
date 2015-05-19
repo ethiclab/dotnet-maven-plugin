ef2279c76f4c9d5cd180691db4da27b626256744
Montoya Edu <montoya.edu@gmail.com>
2015-05-19 15:28:17 +0200

    Add Version Mojo. (work in progress.)

--- src/main/java/it/ethiclab/dotnet/VersionMojo.java
+++ src/main/java/it/ethiclab/dotnet/VersionMojo.java
@@ -1,17 +1,18 @@
 package it.ethiclab.dotnet;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-@Mojo(name = "init", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
-public class InitMojo extends AbstractMojo {
+@Mojo(name = "version")
+public class VersionMojo extends AbstractMojo {
 	@Parameter(defaultValue = "${project}", readonly = true)
 	private MavenProject project;
 
@@ -19,7 +20,7 @@ public class InitMojo extends AbstractMojo {
     {
     	Log log = getLog();
     	try {
-    		log.info("INIT BEGIN");
+    		log.info("VERSION BEGIN");
     		
     		log.info(project.getGroupId());
     		log.info(project.getArtifactId());
@@ -31,8 +32,41 @@ public class InitMojo extends AbstractMojo {
     		}
     		
     		log.info("BASE DIR = " + project.getBasedir());
+    		
+    		visit(project.getBasedir());
+    		
     	} finally {
-    		log.info("INIT END");
+    		log.info("VERSION END");
     	}
     }
+
+	private void visit(File f) {
+		if (f.isDirectory()) {
+			if (f.getName().compareToIgnoreCase("bin") == 0) {
+				return;
+			}
+			if (f.getName().compareToIgnoreCase("obj") == 0) {
+				return;
+			}
+			if (f.getName().compareToIgnoreCase("target") == 0) {
+				return;
+			}
+			if (f.list() != null) {
+				for (String s : f.list()) {
+					File ff = Paths.get(f.getAbsolutePath(), s).toFile();
+					if (ff.isFile()) {
+						onFile(ff);
+					} else {
+						visit(ff);
+					}
+				}
+			}
+		}
+	}
+
+	private void onFile(File f) {
+		if (f.getName().endsWith(".cs")) {
+			getLog().info("processing " + f.getAbsolutePath());
+		}
+	}
 }
